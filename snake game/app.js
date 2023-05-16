340e62bfb635f60cbb74631d6d4953bcc2b65fbd
Mhlfka <129636098+Mhlfka@users.noreply.github.com>
2023-05-16 11:53:50 +0500

    Add files via upload

--- snake game/app.js
+++ snake game/app.js
@@ -1,4 +1,6 @@
-
+playsound();
+gameover();
+back();
 //board
 
 var blockSize = 25;
@@ -31,12 +33,16 @@ var gameOver = false;
 
 
 
+
+
+
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = rows*blockSize;
     board.width = cols*blockSize;
     context = board.getContext("2d"); //used for drawing on the board
-
+    back ();
     placeFood();
     document.addEventListener("keyup", changeDirection);
     // update();
@@ -54,8 +60,12 @@ function update() {
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
     if(snakeX == foodX && snakeY == foodY){
+        playsound();
         snakeBody.push([foodX, foodY]);
+        
         placeFood();
+
+
     }
 
     for (let i = snakeBody.length-1; i > 0; i--) {
@@ -77,13 +87,16 @@ function update() {
 
     //game over conditions
     if(snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameover();
         gameOver = true;
+       
         alert("Game over, man");
     }
 
     for (let i = 0; i < snakeBody.length; i++){
         if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
             gameOver = true;
+            gameover();
             alert("Game over,man");
 
         }
@@ -121,7 +134,20 @@ function placeFood() {
 }
 
 
+function playsound () {
+    let beat =  new Audio ('heavy_swallowwav-14682.mp3')
+    beat.play()
+}
 
+function gameover () {
+    let beat2 =  new Audio ('negative_beeps-6008.mp3')
+    beat2.play()
+}
+
+function back () {
+    let beat3 =  new Audio ('the-desert-10037.mp3')
+    beat3.play()
+}
 
 
 // if (e.code == "ArrowUp" && velocityY != 1) {
